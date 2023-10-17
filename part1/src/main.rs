45a33484e6cdcb328a91ec569ed0edb291aa640a
bouhaniHamdi <bouhanihamdi@gmail.com>
2023-10-17 14:15:56 +0200

    up

--- part1/src/main.rs
+++ part1/src/main.rs
@@ -1,5 +1,17 @@
+struct Point {
+    x: i32,
+    y: i32,
+}
 
 // one binary crates
 fn main() {
-    println!("Hello, world!");
+    let origin: Point = Point { x: 0, y: 0 };
+
+    // A struct with unnamed fields, called a ‘tuple struct’
+    struct Point2(i32, i32);
+
+    let origin2 = Point2(0, 0);
+
+    println!("The origin is at ({}, {})", origin.x, origin.y);
+    println!("The origin is at ({}, {})", origin2.0, origin2.1);
 }
