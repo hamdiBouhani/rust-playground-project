31051b3d970b904127db60abef1f7223c26c2bf6
bouhaniHamdi <bouhanihamdi@gmail.com>
2023-10-17 14:20:32 +0200

    up

--- part1/src/main.rs
+++ part1/src/main.rs
@@ -3,6 +3,30 @@ struct Point {
     y: i32,
 }
 
+// Generics //
+
+struct Foo<T> {
+    bar: T,
+}
+
+// Methods //
+
+impl<T> Foo<T> {
+    // Methods take an explicit `self` parameter
+    fn bar(&self) -> &T {
+        // self is borrowed
+        &self.bar
+    }
+    fn bar_mut(&mut self) -> &mut T {
+        // self is mutably borrowed
+        &mut self.bar
+    }
+    fn into_bar(self) -> T {
+        // here self is consumed
+        self.bar
+    }
+}
+
 // one binary crates
 fn main() {
     let origin: Point = Point { x: 0, y: 0 };
@@ -14,4 +38,7 @@ fn main() {
 
     println!("The origin is at ({}, {})", origin.x, origin.y);
     println!("The origin is at ({}, {})", origin2.0, origin2.1);
+
+    let a_foo = Foo { bar: 1 };
+    println!("{}", a_foo.bar()); // 1
 }
