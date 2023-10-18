0294a96741f2470c85d805b2ac15334ea835c453
bouhaniHamdi <bouhanihamdi@gmail.com>
2023-10-18 10:48:07 +0200

    testing btokio

---
+++ tokio-tutorial/curl-post.sh
@@ -0,0 +1,20 @@
+curl --request POST 'http://127.0.0.1:8181/150.50'
+
+echo "GET balance"
+
+curl 'http://127.0.0.1:8181/' \
+  -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7' \
+  -H 'Accept-Language: en-US,en;q=0.9,fr;q=0.8,ar;q=0.7,pl;q=0.6' \
+  -H 'Cache-Control: max-age=0' \
+  -H 'Connection: keep-alive' \
+  -H 'Referer: https://jbarszczewski.hashnode.dev/' \
+  -H 'Sec-Fetch-Dest: document' \
+  -H 'Sec-Fetch-Mode: navigate' \
+  -H 'Sec-Fetch-Site: cross-site' \
+  -H 'Sec-Fetch-User: ?1' \
+  -H 'Upgrade-Insecure-Requests: 1' \
+  -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36' \
+  -H 'sec-ch-ua: "Chromium";v="118", "Google Chrome";v="118", "Not=A?Brand";v="99"' \
+  -H 'sec-ch-ua-mobile: ?0' \
+  -H 'sec-ch-ua-platform: "Linux"' \
+  --compressed
